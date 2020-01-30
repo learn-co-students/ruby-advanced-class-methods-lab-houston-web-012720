3757a9b2fb28e9a48166e8e158dde40e954bccb4
DrVonDevious <drvondevious@gmail.com>
2020-01-30 15:24:26 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
+
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
+
+  def initialize(name = nil, artist_name = nil)
+    @name = name 
+    @artist_name = artist_name
+  end  
 
   def self.all
     @@all
@@ -10,4 +17,50 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song 
+    song
+  end 
+
+  def self.new_by_name(name)
+    Song.new(name)
+  end 
+
+  def self.create_by_name(name, artist = nil)
+    song = Song.new(name)
+    @@all << song
+    song
+  end  
+
+  def self.find_by_name(name)
+    Song.all.find { |song| song.name == name }
+  end 
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    song ? song : self.create_by_name(name)
+  end  
+
+  def self.alphabetical
+    Song.all.sort_by { |song| song.name }
+  end  
+
+  def self.new_from_filename(file)
+    artist, song, ext = file.split(/[-.]/)
+    Song.new(song.strip, artist.strip)
+  end  
+
+  def self.create_from_filename(file)
+    artist, title, ext = file.split(/[-.]/)
+    song = Song.new(title.strip, artist.strip)
+    @@all << song
+    song
+  end 
+
+  def self.destroy_all
+    @@all.clear
+  end  
+
 end
+
